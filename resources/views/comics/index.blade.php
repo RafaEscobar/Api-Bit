dcae69b4ea9686685b4041cc5f2ba46d83558ca5
Alin Cardenas <al222110745@gmail.com>
2023-05-09 10:05:27 -0600

    Implementacion de bootstrap

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,5 +7,11 @@
     @foreach ($comics->data->results as $comic)
             <li>{{ $comic->title }}</li>
     @endforeach
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
 
 @endsection
