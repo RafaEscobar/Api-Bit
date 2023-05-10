f4c04cf227ebafb012095ddc1b41f1a3818dbd98
Alin Cardenas <al222110745@gmail.com>
2023-05-10 10:58:56 -0600

    implementacion de formularios

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'login')
     
 @section('content')
-
-
+    <body>
+        <section class="form-register bg-dark">
+            <h3>Login</h3><br>
+            <input class="register bg-dark" type="email" name="email"  placeholder="Ingresa email" required>
+            <input class="register bg-dark" type="password" name="password"  placeholder="Ingresa contraseña" required>
+            <button type="submit" class="btn btn-outline-light">Registrar</button>
+        </section>
+    </body>
 @endsection
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -3,5 +3,19 @@
 @section('title', 'registro')
     
 @section('content')
+<body>
+
+    <section class="form-register bg-dark">
+        <h3>Registro</h3><br>
+        <input class="register bg-dark" type="text" name="nombre"  placeholder="Ingresa nombre" required>
+        <input class="register bg-dark" type="text" name="apellidoA" placeholder="Ingresa apellido paterno" required>
+        <input class="register bg-dark" type="text" name="apellidoB"  placeholder="Ingresa apellido materno" required>
+        <input class="register bg-dark" type="email" name="email"  placeholder="Ingresa email" required>
+        <input class="register bg-dark" type="password" name="password"  placeholder="Ingresa contraseña" required>
+        <input class="file " type="file" name="fotoPerf" /> 
+        <button type="submit" class="btn btn-outline-light">Registrar</button>
+    </section>
+    
+</body>
 
 @endsection
